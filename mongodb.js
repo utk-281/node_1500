ca523e9d5beedf69e36a859e23ecbe1c55477540
utkarsh <utkarshgupta.281@gmail.com>
2025-02-13 16:58:05 +0530

    add comparison operators

--- mongodb.js
+++ mongodb.js
@@ -1,10 +1,26 @@
-db.user.updateOne(
-  { name1: "raju1" },
-  { $set: { address: "Noida", phoneNo: 1234567890, email: "value" } }
-);
+// // db.user.updateOne(
+// //   { name1: "raju1" },
+// //   { $set: { address: "Noida", phoneNo: 1234567890, email: "value" } }
+// // );
 
-function sum(a, b) {
-  return a + b;
-}
+// // function sum(a, b) {
+// //   return a + b;
+// // }
 
-console.log(sum(1, 2));
+// // console.log(sum(1, 2));
+
+// // db.emp.find({ job: "manager" }, { sal: { $gt: 1200 } });
+
+// // db.emp.updateOne({ eName: "jones" }, { $rename: { Salary: "sal" } });
+
+// if ("") {
+//   console.log("truthy");
+// } else {
+//   console.log("falsy");
+// }
+
+// db.emp.find({ job: "clerk", job: "manager", sal: { $gt: 3200 } });
+
+// db.emp.find({ $and: [{ job: "manager" }, { sal: { $gt: 1200 } }] });
+
+// db.emp.find({ $and: [{ sal: { $gt: 1200 } }, { sal: { $lt: 2800 } }] });
