2c82bad389fe2823a79d2244012cbec07013883e
utkarsh <utkarshgupta.281@gmail.com>
2025-02-18 10:06:02 +0530

    add mongodb module

--- NodeJS/Modules/Third Party Modules/MongoDB/index.js
+++ NodeJS/Modules/Third Party Modules/MongoDB/index.js
@@ -20,30 +20,57 @@
 //? package-lock.json ==> it stores the meta data of the installed modules like version, integrity etc..
 //? dependencies ==> it stores the name and version of the installed modules
 
-// let mongodb = require("mongodb").MongoClient
-let { MongoClient } = require("mongodb");
-//! MongoClient => it helps us to connect to the database
-// console.log(mongodb);
+let mongodb = require("mongodb");
+// console.log(mongodb.MongoClient);
 
-// let result = MongoClient.connect("mongodb://localhost:27017");
+//! MongoClient ==> it helps to connect the nodeJS project with database
+//! to connect with the database there is a method called as connect("url of database")
 
 let connectDB = async () => {
-  //! use connect() to connect to the database
-  const client = await MongoClient.connect("mongodb://localhost:27017");
-  //   console.log(result);
-  console.log("connection established");
+  let client = await mongodb.MongoClient.connect("mongodb://localhost:27017");
+  // console.log(client);
+  // console.log(client.db);
+  console.log("database connection successful");
 
-  //! create a database using the client variable
-  //! use db("database-name") to create a database
-  let database = client.db("NodeDB");
-  //   console.log(database);
+  //! create a database, using db("database-name"),
+  let database = client.db("myDb");
+  // console.log(database);
+  // console.log(database.createCollection);
   console.log("database created");
 
-  //! create a collection using database variable
-  //! use createCollection("collection-name")
-  let collection = await database.createCollection("users");
+  //! create a collection using createCollection("collection-name")
+  let collection = await database.createCollection("myCollection");
+  // console.log(collection);
+  // console.log(collection.insertOne);
+  // console.log(collection.insertMany);
   console.log("collection created");
 
-  //   collection.insertMany([{ name: "abc" }]);
+  //! ================== CRUD OPERATIONS ==================
+
+  //!  inserting single document
+  // let result = await collection.insertOne({ name: "def", age: 20 });
+  // console.log("data inserted");
+  // console.log(result);
+
+  //! inserting multiple documents
+  // let result = await collection.insertMany([{ name: "xyz" }, { age: 57 }]);
+  // console.log("Data inserted");
+  // console.log(result);
+
+  //! fetching single document
+  // let result = await collection.findOne({ name: { $eq: "abc" } });
+  // console.log(result);
+
+  //! fetching multiple docs
+  // let result = collection.find(); // --> cursor object
+  // let docs = await result.toArray();
+  // console.log(docs);
+  // let result = await collection.find().toArray();
+  // console.log(result);
+
+  // let result = collection.find();
+  // result.forEach((doc) => {
+  //   console.log(doc);
+  // });
 };
 connectDB();
